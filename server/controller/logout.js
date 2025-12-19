c5f7101f98106d4283bbb01bdf908d966b227934
Akash Yadav <akashtechmedia01@gmail.com>
2025-12-19 21:21:23 +0530

    Add vercel.json and fix logout cookie

--- server/controller/logout.js
+++ server/controller/logout.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 const logout = async(req, res)=>{
     try {
-        res.clearCookie("token", {httpOnly : true, secure : process.env.NODE_ENV === "production", sameSite : "lax"});
+        res.clearCookie("token", {httpOnly : true, secure : true, sameSite : "none"});
         res.status(200).json({ success : true, message : "logout successfully"});
     } catch (error) {
         return res.status(500).json({message : error.message});
